46e93afaac098d11fbf5efeccecf1d14982cf356
Ashlyn Smith <SMITHASHLYN210@YAHOO.COM>
2023-06-06 13:04:01 -0500

    hadlebbars being fixed

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -3,6 +3,7 @@ const { User, Project } = require('../models');
 
 const userData = require('./userData.json');
 const projectData = require('./projectData.json');
+const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -18,6 +19,10 @@ const seedDatabase = async () => {
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };
